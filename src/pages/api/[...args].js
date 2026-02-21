be4ca017c90e6b4d6eff5a6366c8a7a2f330ee5a
visualdendy-ux <visualdendy@gmail.com>
2026-02-22 00:30:14 +0800

    Fix image upload: Disable Next.js body parser for API route

--- src/pages/api/[...args].js
+++ src/pages/api/[...args].js
@@ -1,5 +1,11 @@
 const app = require('../../../backend/src/index');
 
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
 export default function handler(req, res) {
     return app(req, res);
 }
